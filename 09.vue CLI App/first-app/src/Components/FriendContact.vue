c0016ee2460fe369eb583bc127d4cb5e3a23c9a2
gokul sai <gokulsai321@gmail.com>
2022-10-09 10:24:03 +0530

    components in vue cli

---
+++ 09.vue CLI App/first-app/src/Components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <header v-if="! isVisible">
+      <h1>Name: {{name.name}}</h1>
+      <h2>Age: {{name.Age}}</h2>
+      <h2>Email: {{name.email}}</h2>
+      <h3>Phone number: {{name.phone_number}}</h3>
+    </header>
+    <button @click="toggleVisible">{{isVisible?"Show":"Hide"}} details</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: {
+        name: "Gokul Sai",
+        Age: 19,
+        email: "gokulsai321@gmail.com",
+        phone_number: "5646546826"
+      },
+      isVisible: false
+    };
+  },
+
+  methods: {
+    toggleVisible() {
+      this.isVisible = !this.isVisible;
+    }
+  }
+};
+</script>
---
+++ 10.counter app using vue-cli/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <h2>Count: {{count}}</h2>
+        <button @click="increment">Increment</button>
+        <button @click="decrement">Decrement</button>
+    </section>
+    
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            count : 0
+        }
+    },
+    methods: {
+        increment(){
+            this.count++
+        },
+        decrement(){
+            this.count--
+        }
+    }
+}
+</script>
