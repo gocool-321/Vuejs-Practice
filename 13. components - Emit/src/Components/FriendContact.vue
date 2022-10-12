7eede00f4b84ef81f6328591c7d616f35a0a70bd
gokul sai <gokulsai321@gmail.com>
2022-10-12 16:50:57 +0530

    components :emit -> child to parent communication"

---
+++ 13. components - Emit/src/Components/FriendContact.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1>{{name}} {{isFavorite ? "(Favorite)": ""}}</h1>
+    <button @click="toggleVisible">{{isVisible?"Hide":"Show"}} details</button>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <hr />
+    <header v-if=" isVisible">
+      <h1>Name: {{name}}</h1>
+      <h2>Age: {{age}}</h2>
+      <h2>Email: {{email}}</h2>
+      <h3>Phone number: {{phone}}</h3>
+      <hr />
+      <hr />
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  // props: ["name", "phone", "email", "age"],
+  props: {
+    name: {
+      type: String,
+      require: true
+    },
+    phone: {
+      type: String,
+      require: true
+    },
+    email: {
+      type: String,
+      require: true
+    },
+    age: {
+      type: Number,
+      require: false,
+      default: 19
+    }
+  },
+  data() {
+    return {
+      isVisible: false,
+      isFavorite: false
+    };
+  },
+
+  methods: {
+    toggleVisible() {
+      this.isVisible = !this.isVisible;
+    },
+    toggleFavorite() {
+      this.isFavorite = !this.isFavorite;
+    }
+  }
+};
+</script>
