f645dea3765c253c04229aeed505929eb58c5758
gokul sai <gokulsai321@gmail.com>
2022-10-15 15:21:46 +0530

    slots!!!

---
+++ 17.slots/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <card>
+    <template v-slot:subtitle>
+      <p>Hey There!</p>
+    </template>
+    <h1>{{word[0]}}</h1>
+  </card>
+</template>
+
+<script>
+import card from "./components/card-item.vue";
+export default {
+  components: { card },
+  data() {
+    return {
+      word: ["Hello", "World"]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ 17.slots/src/components/card-item.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card item">
+    <div class="card-body">
+      <h6 class="card-subtitle mb-2 text-muted">
+        <slot name="subtitle">
+          <p>Hi There!</p>
+        </slot>
+      </h6>
+      <div class="card-text">
+        <slot>
+          <p>Here is some Body</p>
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.item {
+  width: 50vw;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 2rem;
+}
+</style>
