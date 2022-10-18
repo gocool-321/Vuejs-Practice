34d1dd5275d384de06faced1e99c2b5835a30acf
gokul sai <gokulsai321@gmail.com>
2022-10-18 13:58:04 +0530

    added 404 not found page & redirect

---
+++ 22.routes blue print/src/components/notFound.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <section class="vh-100" style="background-color: #eee;">
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col col-lg-9 col-xl-7">
+            <div class="card" style="border-radius: 15px;">
+              <div class="card-body p-5">
+                <div class="text-center mb-4 pb-2">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-quotes/bulb.webp"
+                    alt="Bulb"
+                    width="100"
+                  />
+                </div>
+
+                <figure class="text-center mb-0">
+                  <blockquote class="blockquote">
+                    <p class="pb-3">
+                      <i class="fas fa-quote-left fa-xs text-primary"></i>
+                      <span class="lead font-italic">{{joke}}</span>
+                      <i class="fas fa-quote-right fa-xs text-primary"></i>
+                    </p>
+                  </blockquote>
+                  <h6>Will load new joke in {{this.time}} seconds</h6>
+                  <div class="progress">
+                    <div
+                      class="progress-bar"
+                      role="progressbar"
+                      :style="{width: progressVal}"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    >{{progressVal}}</div>
+                  </div>
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      time: 20,
+      joke: 'Joke is the Joke of the Joke is called the Joke!'
+    };
+  },
+  methods: {
+    getJokes: async that => {
+      const { data } = await axios.get(
+        'https://v2.jokeapi.dev/joke/Any?type=single'
+      );
+      that.joke = data.joke;
+    }
+  },
+  computed: {
+    progressVal() {
+      return 100 - Math.round((this.time / 20) * 100) + '%';
+    }
+  },
+  watch: {
+    time(newValue) {
+      if (newValue == 0) {
+        this.time = 20;
+        this.getJokes(this);
+      }
+    }
+  },
+  mounted() {
+    this.getJokes(this);
+    setInterval(() => {
+      this.time -= 1;
+    }, 1000);
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- 22.routes blue print/src/main.js
+++ 22.routes blue print/src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-import Home from './components/home/home.vue';
+// import Home from './components/home/home.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import TeamMember from './components/teams/TeamMembers.vue';
 import UsersList from './components/users/UsersList.vue';
+import NotFound from './components/notFound.vue';
 
 import App from './App.vue';
 
@@ -12,10 +13,11 @@ const app = createApp(App);
 
 const router = createRouter({
   routes: [
-    { path: '/', component: Home },
+    { path: '/', component: NotFound },
     { path: '/teams', component: TeamsList },
     { path: '/teams/:teamId', component: TeamMember, props: true },
     { path: '/users', component: UsersList },
+    { path: '/:notfound(.*)', component: NotFound },
   ],
   history: createWebHistory(),
   linkActiveClass: 'active',
